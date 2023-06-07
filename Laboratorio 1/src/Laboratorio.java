d7b8c3a1c401d224fdc118bfa0b5a4831f115526
Samuel9930 <110494912+Samuel9930@users.noreply.github.com>
2023-06-06 23:54:09 -0500

    Add files via upload

---
+++ Laboratorio 1/src/Laboratorio.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+
+public class Laboratorio {
+	
+		int numer;
+		int i, neg = 0, par = 0;
+
+		public void entrada() {
+			for (i = 0; i < 5; i++) {
+				numer = ingresarDatos("Digite un número: " + (i+1));
+				if (numer % 2 == 0) {
+					par++;
+				}
+
+				if (numer < 0) {
+					neg++;
+				}
+			}
+			JOptionPane.showMessageDialog(null,
+					"El número de números pares es: " + par + "\n" + "El número de números negativos es: " + neg);
+		}
+
+		public static int ingresarDatos(String mensaje) {
+			String cadena;
+			int valor;
+			cadena = JOptionPane.showInputDialog(null, mensaje);
+			valor = Integer.parseInt(cadena);
+			return valor;
+		}
+	
+}
---
+++ Laboratorio 1/src/Principal.java
@@ -0,0 +1,11 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Laboratorio numero = new Laboratorio();
+		numero.entrada();
+
+
+	}
+
+}
